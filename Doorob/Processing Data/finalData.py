2dc45a9cc99ea77d2e41eaeee66cb2efa20573ff
RemanAlwadiee <riman.alwadiee03@gmail.com>
2024-10-18 18:55:16 +0300

    comments

--- Doorob/Processing Data/finalData.py
+++ Doorob/Processing Data/finalData.py
@@ -1,18 +1,18 @@
+# We used thhis code to compine the 2 dataset into one csv file 
+
 import pandas as pd
 
-# Load the two CSV files
+
 file1 = r'C:\Users\riman\OneDrive\Documents\updated_places.csv'
 file2 = r'C:\Users\riman\Downloads\combined_file (5).csv'
 
-# Read the CSV files into dataframes
+
 df1 = pd.read_csv(file1)
 df2 = pd.read_csv(file2)
 
-# Filter df2 to only include the desired 'type' values
 desired_types = ['zoo', 'shopping_mall', 'park', 'hotel', 'art_gallery']
 df2_filtered = df2[df2['type'].isin(desired_types)]
 
-# Prepare a new dataframe with matching columns from df2 to be added into df1
 df2_filtered['granular_category'] = df2_filtered['type']
 df2_filtered['place_name'] = df2_filtered['name']
 df2_filtered['average_rating'] = df2_filtered['rating']
@@ -20,7 +20,6 @@ df2_filtered['lat'] = df2_filtered['lat']
 df2_filtered['lng'] = df2_filtered['lng']
 df2_filtered['place_id'] = df2_filtered['place_id']
 
-# Only add the extra columns if they exist in df1
 if 'rate_count' in df1.columns:
     df2_filtered['rate_count'] = 'N/A'
 if 'is_restaurant' in df1.columns:
@@ -28,14 +27,11 @@ if 'is_restaurant' in df1.columns:
 if 'categories' in df1.columns:
     df2_filtered['categories'] = 'N/A'
 
-# Select the columns in df2_filtered that match those in df1
 matching_columns = [col for col in df1.columns if col in df2_filtered.columns]
 df2_filtered = df2_filtered[matching_columns]
 
-# Append df2_filtered to df1
 df_combined = pd.concat([df1, df2_filtered], ignore_index=True)
 
-# Save the combined dataframe to a new CSV file
 output_file = r'C:\Users\riman\Documents\combined_updated_places.csv'
 df_combined.to_csv(output_file, index=False)
 
--- Doorob/Processing Data/fixName.py
+++ Doorob/Processing Data/fixName.py
@@ -1,15 +1,16 @@
+# Read the CSV file, skipping the first 8838 rows (since row index starts at 0) and encoding='utf-8-sig'
+
+
 import pandas as pd
 
-# Specify the file path
+
 file_path = r'C:\Users\riman\Desktop\combined_updated_places.csv'
 
-# Read the CSV file, skipping the first 8838 rows (since row index starts at 0)
 df = pd.read_csv(file_path, encoding='utf-8-sig', skiprows=8838)
 
-# Display the first few rows after skipping
+
 print(df.head())
 
-# Optionally, save it back after processing or modifications (if needed)
 df.to_csv(file_path, index=False, encoding='utf-8-sig')
 
 print(f"File processed and saved successfully starting from row 8839 at {file_path}")
