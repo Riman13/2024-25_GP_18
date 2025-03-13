0c98d6d3bc29aae1320c2ea11b765e8cba1061b5
leenaksu <leenaalz2021@gmail.com>
2025-03-13 16:31:31 +0300

    bookmark

--- Doorob/templates/homepage.php
+++ Doorob/templates/homepage.php
@@ -500,7 +500,7 @@ document.addEventListener("DOMContentLoaded", function () {
       <div class="left-section">
       <!--  <p><strong>Category:</strong> <span id="placeCategory"></span></p>-->
         <p><strong>Granular Category:</strong> <span id="placeGranularCategory"></span></p><br>
-        <button class="favorite-btn"><i class="fas fa-bookmark"></i> Bookmak This Place</button>
+        <button class="favorite-btn"><i class="fas fa-bookmark"></i> Bookmark This Place</button>
       </div>
       <div class="divider"></div>
       <div class="right-section">
